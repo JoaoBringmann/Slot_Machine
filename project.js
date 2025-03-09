6eb77b7eceee0fee78be5f842296cd64de8e7b87
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2025-03-09 16:12:43 -0300

    21:11

---
+++ project.js
@@ -0,0 +1,36 @@
+// 1 - Depositar dinheiro
+// 2 - Determinar numero de linhas escolhidas
+// 3 - Coletar dinheiro
+// 4 - Girar a maquina
+// 5 - Checar se o usuario ganhou
+// 6 - Dar o dinheiro de o usuario ganhou
+// 7 - Jogar de novo
+
+const prompt = require("prompt-sync")();
+
+const deposito = () => {
+  while (true) {
+    const depositoValor = prompt("Entre um valor para depositar: ");
+    const numeroDepositoValor = parseFloat(depositoValor);
+    if (isNaN(numeroDepositoValor) || numeroDepositoValor <= 0) {
+      console.log("Valor invalido, tente novamente");
+    } else {
+      return numeroDepositoValor;
+    }
+  }
+};
+
+const pegarNumeroDeLinhas = () => {
+  while (true) {
+    const linhas = prompt("Entre o numero de linhas para apostar (1-3): ");
+    const numeroLinhas = parseFloat(linhas);
+    if (isNaN(numeroLinhas) || numeroLinhas <= 0 || numeroLinhas >= 3) {
+      console.log("Valor invalido, tente novamente");
+    } else {
+      return numeroLinhas;
+    }
+  }
+};
+
+deposito();
+pegarNumeroDeLinhas();
